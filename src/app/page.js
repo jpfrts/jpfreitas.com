cd4fdd607c457d849bd4918e2cc2a4c1e12a0b6e
JP Freitas <joaopedrof@icloud.com>
2023-05-15 03:27:43 -0300

    feat: schema.org

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,23 @@
+import { SocialProfileJsonLd } from "next-seo";
 import { Layout } from "./components/layout";
 
 export default function Home() {
-  return <Layout />;
+  return (
+    <>
+      <Layout />;
+      <SocialProfileJsonLd
+        useAppDir={true}
+        type="Person"
+        name="jp freitas"
+        url="https://jpfreitas.com"
+        sameAs={[
+          "https://twitch.tv/jpfreitas_",
+          "https://instagram.com/jpfreitas_",
+          "https://youtube.com/@jp.freitas",
+          "https://tiktok.com/@jpfreitas_",
+          "https://twitter.com/@jpfreitas_",
+        ]}
+      />
+    </>
+  );
 }
